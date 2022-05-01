5e6d7876db30c9b0c557d5024247e20eee1cd9a1
heum-ji <dragon2009t@gmail.com>
2022-05-02 00:00:25 +0900

    Docs : Components 파일 분리

--- 생활코딩/WEB2/react-app/src/App.js
+++ 생활코딩/WEB2/react-app/src/App.js
@@ -1,49 +1,8 @@
 import { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import "./App.css";
-
-class Subject extends Component {
-  render() {
-    // class 내부 함수는 Function 생략
-    return (
-      // Component는 하나의 최상위 태그로 시작되어야 함
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
 
 class App extends Component {
   render() {
---
+++ 생활코딩/WEB2/react-app/src/components/Content.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Content extends Component {
+    render() {
+      return (
+        <article>
+          <h2>{this.props.title}</h2>
+          {this.props.desc}
+        </article>
+      );
+    }
+  }
+
+  export default Content; // 외부에서 사용할 수 있게 설정
