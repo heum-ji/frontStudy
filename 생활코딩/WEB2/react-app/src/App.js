786b90ac38afbe97156fff7fac15c8a8408f2b2f
heum-ji <dragon2009t@gmail.com>
2022-05-02 00:29:44 +0900

    Feat : state 사용
    state : 외부에서 알 수 없게 함(캡슐화 개념?)
    -> 생성자에서 this.state로 초기화
    -> 외부에서 this.state로 접근

--- 생활코딩/WEB2/react-app/src/App.js
+++ 생활코딩/WEB2/react-app/src/App.js
@@ -5,13 +5,31 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    // 생성자 : render() 보다 먼저 실행됨 - 초기화
+    super(props);
+    this.state = {
+      // state 값 초기화
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
+  // class 내부 함수는 Function 생략
   render() {
     return (
+      // Component는 하나의 최상위 태그로 시작되어야 함
       // Create React App : JSX -> JS로 Converting 해줌
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title} // 상위 컴포넌트 -> 하위 컴포넌트 state 사용
+          sub={this.state.subject.sub}
+        ></Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
           desc="HTML is HyperText Markup Language."
--- 생활코딩/WEB2/react-app/src/components/Subject.js
+++ 생활코딩/WEB2/react-app/src/components/Subject.js
@@ -1,16 +1,14 @@
 import { Component } from "react";
 
 class Subject extends Component {
-    render() {
-      // class 내부 함수는 Function 생략
-      return (
-        // Component는 하나의 최상위 태그로 시작되어야 함
-        <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
-        </header>
-      );
-    }
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
   }
+}
 
-  export default Subject;
+export default Subject;
