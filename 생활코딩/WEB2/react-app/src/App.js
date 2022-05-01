6ae77385b5a46877a297836f7ddf23703d229515
heum-ji <dragon2009t@gmail.com>
2022-05-01 23:15:10 +0900

    Feat : Component 생성

--- 생활코딩/WEB2/react-app/src/App.js
+++ 생활코딩/WEB2/react-app/src/App.js
@@ -1,36 +1,61 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+class Subject extends Component {
   render() {
+    // class 내부 함수는 Function 생략
     return (
-      <div className="App">
-        Hello, React!!
-      </div>
+      // Component는 하나의 최상위 태그로 시작되어야 함
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
     );
   }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      // Create React App : JSX -> JS로 Converting 해줌
+      <div className="App">
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
+      </div>
+    );
+  }
+}
 
 export default App;
